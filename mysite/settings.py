ac54d6d8de85137e70b3013276a2ead4d1b903dd
asyaaaa17 <asyaaaa17@gmail.com>
2019-02-24 23:24:27 +0200

    false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bg^p6r_n#(ffih9wjtbvw%$x3#q^jfl(o=gn_zcy9=(r)i&^f%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'asya17.pythonanywhere.com']
 
